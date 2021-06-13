e6ff20253ee489a1f6a8b83e4144f1991b5d59b2
deepaksharma <ds284122@gmail.com>
2021-06-13 16:57:24 +0530

    Add array and button

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:vars
+    
+    let button = UIButton()
+    var array:[String] = []
+    
+    
     //MARK: View did life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        
+        
         
         self.title = "ViewController"
     }
